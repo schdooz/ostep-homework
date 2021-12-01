5b05ff19d6f528a20dce158cf3660e986c84fce5
Mark Dinh <mark_dinh@hotmail.co.uk>
2021-12-01 13:28:35 +0000

    Compute fairness prob distribution when quanta = 2.

--- cpu-sched-lottery/fairness-probability-distribution.c
+++ cpu-sched-lottery/fairness-probability-distribution.c
@@ -1,6 +1,6 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 long double factorial(int n) {
   if (n < 0) {
@@ -12,7 +12,7 @@ long double factorial(int n) {
     return 1;
   }
 
-  return (long double) n * factorial(n - 1);
+  return (long double)n * factorial(n - 1);
 }
 
 int main() {
@@ -20,14 +20,29 @@ int main() {
   long double p;
   float expectation = 0;
 
+  printf("quanta q = 1\n");
+
   for (i = 1; i < 101; i++) {
-    p = pow(0.5, 199 - i) * factorial(199 - i) / (factorial(100 - i) * factorial(99));
-    expectation += (float) p * i;
+    p = pow(0.5, 199 - i) * factorial(199 - i) /
+        (factorial(100 - i) * factorial(99));
+    expectation += (float)p * i;
     printf("P(X = %3d) = %10.3LE\n", i, p);
+  }
+
+  printf("E(X) = %.3f\n", expectation);
+
+  expectation = 0;
+
+  printf("quanta q = 2\n");
+
+  for (i = 1; i < 51; i++) {
+    p = pow(0.5, 99 - i) * factorial(99 - i) /
+        (factorial(50 - i) * factorial(49));
+    expectation += (float)p * 2 * i;
+    printf("P(X = %3d) = %10.3LE\n", 2 * i, p);
   }
 
   printf("E(X) = %.3f\n", expectation);
 
   exit(0);
 }
-
