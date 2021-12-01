e197d79c9eec8b5df08819a627f14ac19d2155b8
Mark Dinh <mark_dinh@hotmail.co.uk>
2021-12-01 11:38:01 +0000

    Compute expectation in fairness-probability-distribution.c.

--- cpu-sched-lottery/fairness-probability-distribution.c
+++ cpu-sched-lottery/fairness-probability-distribution.c
@@ -18,15 +18,15 @@ long double factorial(int n) {
 int main() {
   int i;
   long double p;
-  float sum = 0;
+  float expectation = 0;
 
   for (i = 1; i < 101; i++) {
     p = pow(0.5, 199 - i) * factorial(199 - i) / (factorial(100 - i) * factorial(99));
-    sum += (float) p;
+    expectation += (float) p * i;
     printf("P(X = %d) = %.20Lf\n", i, p);
   }
 
-  printf("sum is %.20f\n", sum);
+  printf("E(X) = %.20f\n", expectation);
 
   exit(0);
 }
