2b4c0f23e861522dc9c12474e479cf9529015631
Mark Dinh <mark_dinh@hotmail.co.uk>
2022-07-24 02:28:13 +0100

    Modify main() for vector.c.

--- vm-api/vector.c
+++ vm-api/vector.c
@@ -85,14 +85,35 @@ int main() {
     struct vector v = vector_ctor();
     unsigned char c;
 
-    vector_add(&v, 'a');
-    vector_add(&v, 'b');
-    vector_add(&v, 'c');
+    vector_add(&v, '0');
+    vector_add(&v, '1');
+    vector_add(&v, '2');
+    vector_add(&v, '3');
+    vector_add(&v, '4');
+    vector_add(&v, '5');
+    vector_add(&v, '6');
+    vector_add(&v, '7');
 
     vector_get(&v, 0, &c);
     printf("First element is %c\n", c);
 
     vector_get(&v, 1, &c);
     printf("Second element is %c\n", c);
+
+    printf("count is %lu\n", v.count);
+    printf("buflen is %lu\n", v.buflen);
+
+    vector_pop(&v, &c);
+    printf("Popped value is %c\n", c);
+    printf("count after popping is %lu\n", v.count);
+
+    printf("Popping 4 more times...\n");
+    vector_pop(&v, &c);
+    vector_pop(&v, &c);
+    vector_pop(&v, &c);
+    vector_pop(&v, &c);
+
+    printf("Last popped value is %c\n", c);
+    printf("count is %lu\n", v.count);
     printf("buflen is %lu\n", v.buflen);
 }
